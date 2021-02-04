076698dcd4b5b4eeffca903db15920054fe140ba
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 18:35:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,7 +3,22 @@ phrase = "It's-a me, Mario!"
   puts phrase
 end
 
+
 def toadstool
     status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+
+def link
+  link = "It's Dangerous To Go Alone! Take This."
+  puts link
+end
+
+
+def all_phrases
+mario
+toadstool
+link
+end
+all_phrases
